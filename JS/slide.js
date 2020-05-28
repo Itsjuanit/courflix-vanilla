8ed9c5413d98b2de0b74004da282a8d38039163f
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2020-05-27 22:24:08 -0300

    New repo

---
+++ JS/slide.js
@@ -0,0 +1,23 @@
+const carousels = document.querySelectorAll(".carousels");
+
+    Object.values(carousels).map(carousel => {
+        const slider = new Glide(carousel, {
+            type: "carousel",
+            perView: 4,
+            gap: 10,
+
+            breakpoints: {
+                800: {
+                    perView: 2
+                },
+                480: {
+                    perView: 1
+                }
+            }
+
+        });
+
+
+        slider.mount();
+
+    });
