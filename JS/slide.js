deb35db333d290161fb58d1b5ab7dd4b45f5aec3
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2020-05-28 20:38:04 -0300

    New images for Silicon Valley Craousel and good practices and indentation

--- JS/slide.js
+++ JS/slide.js
@@ -1,23 +1,23 @@
 const carousels = document.querySelectorAll(".carousels");
 
-    Object.values(carousels).map(carousel => {
-        const slider = new Glide(carousel, {
-            type: "carousel",
-            perView: 4,
-            gap: 5,
+Object.values(carousels).map(carousel => {
+    const slider = new Glide(carousel, {
+        type: "carousel",
+        perView: 4,
+        gap: 5,
 
-            breakpoints: {
-                800: {
-                    perView: 2
-                },
-                480: {
-                    perView: 1
-                }
+        breakpoints: {
+            800: {
+                perView: 2
+            },
+            480: {
+                perView: 1
             }
-
-        });
-
-
-        slider.mount();
+        }
 
     });
+
+
+    slider.mount();
+
+});
